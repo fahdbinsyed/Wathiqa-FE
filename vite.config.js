3f937e0eb46344c0f2095063d5faff228af30886
MohammedAdnaanJ <mohamedadnan081@gmail.com>
2026-04-26 19:25:44 +0530

    migrate to vite

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [
+    react({
+      include: /\.(js|jsx|ts|tsx)$/,
+    }),
+  ],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+  },
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      },
+    },
+  },
+});
